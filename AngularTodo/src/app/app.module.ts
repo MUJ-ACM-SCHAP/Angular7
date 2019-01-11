9652658086d97712284557085bb4b0ff7f1c5378
aeser9o1 <aashiskumar986@gmail.com>
2019-01-11 16:20:25 +0530

    reformat

--- AngularTodo/src/app/app.module.ts
+++ AngularTodo/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/**Components*/
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './home/components/dashboard/dashboard.component';
 import { TodoComponent } from './home/components/todo/todo.component';
+import { FirebaseComponent } from './home/components/firebase/firebase.component';
+
+/** Toolbars and material stuff*/
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 import {HttpClientModule} from '@angular/common/http';
 import {MatCardModule} from '@angular/material/card';
-import { FirebaseComponent } from './home/components/firebase/firebase.component';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+/**Firebase */
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
 import { AngularFireDatabase } from '@angular/fire/database';
